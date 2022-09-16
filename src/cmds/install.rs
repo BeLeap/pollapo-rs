f1ee0b3c54bb9b8abe68c566cabbc4b4bcf2bd11
Changseo <changseo.jang@riiid.co>
2022-09-16 18:52:20 +0900

    implement unzip

--- src/cmds/install.rs
+++ src/cmds/install.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use piz::read::*;
 
 use crate::pollapo_yml::PollapoYml;
 
@@ -27,10 +28,14 @@ async fn install_dep_to_cache(
         panic!("Failed to convert {} into bytes: {}", &zipball_url, err);
     }));
 
-    let cache_dir = match cache_dir {
+    let cache_dir_raw = match cache_dir {
         Some(dir) => dir,
         None => "~/.config/pollapo/cache"
     };
+    let cache_dir = shellexpand::full(cache_dir_raw)
+        .unwrap_or_else(|err| {
+            panic!("Failed to resolve {}: {}", cache_dir_raw, err);
+        });
     let file_name = format!("{}/{}.{}", cache_dir, dep, "zip");
     let file_path = std::path::Path::new(&file_name);
     let file_parent = file_path.parent().unwrap();
@@ -51,12 +56,88 @@ async fn install_dep_to_cache(
     file_path.to_path_buf()
 }
 
+fn extract_cache(path: &std::path::PathBuf, target_dir: Option<&str>) {
+    let target_dir_raw = match target_dir {
+        Some(dir) => dir,
+        None => "~/.config/pollapo/cache"
+    };
+    let target_dir_str = shellexpand::full(target_dir_raw)
+        .unwrap_or_else(|err| {
+            panic!("Failed to resolve {}: {}", target_dir_raw, err);
+        });
+    let target_dir = std::path::Path::new(&*target_dir_str);
+
+    let zipball_file = std::fs::read(path)
+        .unwrap_or_else(|err| {
+            panic!("Failed to open {}: {}", path.to_string_lossy(), err);
+        });
+    let zipball = piz::ZipArchive::new(&zipball_file)
+        .unwrap_or_else(|err| {
+            panic!("Malformed zipball {}: {}", path.to_string_lossy(), err);
+        });
+    
+    let zip_tree = as_tree(zipball.entries())
+        .unwrap_or_else(|err| {
+            panic!("Malfored zipball converting {}: {}", path.to_string_lossy(), err);
+        });
+    zip_tree.files().for_each(|entry| {
+        let full_path = target_dir.join(&*entry.path);
+
+        if let Some(parent) = full_path.parent() {
+            std::fs::create_dir_all(parent)
+                .unwrap_or_else(|err| {
+                    panic!("Failed to create parent {}: {}", parent.to_string_lossy(), err);
+                });
+        }
+        let mut reader = zipball.read(entry)
+            .unwrap_or_else(|err| {
+                panic!("Failed to read entry {}: {}", full_path.to_string_lossy(), err);
+            });
+        let mut out = std::fs::File::create(&full_path)
+            .unwrap_or_else(|err| {
+                panic!("Failed to create file {}: {}", full_path.to_string_lossy(), err);
+            });
+        std::io::copy(&mut reader, &mut out)
+            .unwrap_or_else(|err| {
+                panic!("Failed to create {}: {}", full_path.to_string_lossy(), err);
+            });
+    });
+}
+
 #[cfg(test)]
 mod tests {
     use serial_test::file_serial;
 
-    use crate::pollapo_yml::load_pollapo_yml;
+    use crate::{pollapo_yml::load_pollapo_yml, cmds::install::extract_cache};
     use super::install_dep_to_cache;
+
+    #[test]
+    #[file_serial(key, "cache_test/pbkit/interface-pingpong-server@main.zip")]
+    fn extract_cache_should_extract_from_cache_to_target() {
+        // given
+        let pollapo_yml = load_pollapo_yml(Some("pollapo.test.yml"));
+        let path = tokio_test::block_on(
+            install_dep_to_cache(
+                &pollapo_yml,
+                "pbkit/interface-pingpong-server@main", 
+                Some("cache_test"),
+            )
+        );
+
+        // when
+        extract_cache(&path, Some(".pollapo"));
+
+        // then
+        assert!(
+            std::fs::read_dir("./.pollapo/pbkit/interface-pingpong-server").unwrap()
+                .map(|e| e.unwrap().path())
+                .fold(false, |acc, entry| acc || entry.to_string_lossy().contains("pingpong.proto"))
+        );
+
+        // clean
+        std::fs::remove_dir_all("./cache_test/pbkit/interface-pingpong-server@main.zip").unwrap();
+        std::fs::remove_dir_all("./.pollapo/pbkit/interface-pingpong-server").unwrap();
+    }
 
     #[test]
     #[file_serial(key, "cache_test/pbkit/interface-pingpong-server@main.zip")]
@@ -80,33 +161,6 @@ mod tests {
             std::fs::read_dir("./cache_test/pbkit").unwrap()
                 .map(|entry| entry.unwrap().path())
                 .fold(false, |acc, entry| acc || entry.to_string_lossy().contains("pbkit/interface-pingpong-server@main"))
-        );
-
-        // clean
-        std::fs::remove_file("./cache_test/pbkit/interface-pingpong-server@main.zip").unwrap();
-    }
-
-    #[test]
-    #[file_serial(key, "cache_test/pbkit/interface-pingpong-server@main.zip")]
-    fn extract_cache_should_extract_from_cache_to_target() {
-        // given
-        let pollapo_yml = load_pollapo_yml(Some("pollapo.test.yml"));
-        let path = tokio_test::block_on(
-            install_dep_to_cache(
-                &pollapo_yml,
-                "pbkit/interface-pingpong-server@main", 
-                Some("cache_test"),
-            )
-        );
-
-        // when
-        extract_cache(path, Some(".pollapo"));
-
-        // then
-        assert!(
-            std::fs::read_dir("./.pollapo/pbkit/interface-pingpong-server").unwrap()
-                .map(|e| e.unwrap().path())
-                .fold(false, |acc, entry| acc || entry.to_string_lossy().contains("pingpong.proto"))
         );
 
         // clean
