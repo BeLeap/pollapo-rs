eb7657e7ed59b9202b79433e5328145cb2c2fa9d
Changseo <changseo.jang@riiid.co>
2022-09-16 15:33:17 +0900

    add failing test

--- src/pollapo_yml.rs
+++ src/pollapo_yml.rs
@@ -35,6 +35,12 @@ mod tests {
     #[test]
     fn load_pollapo_yml_should_load_root_lock() {
         let pollapo_yml = load_pollapo_yml(Some("pollapo.test.yml"));
-        assert!(pollapo_yml.root.lock["pbkit/interface-pingpong-server@main"] == "58425678c6284305dd09130075cecb54a3a3d32c");
+        assert_eq!(pollapo_yml.root.lock["pbkit/interface-pingpong-server@main"], "58425678c6284305dd09130075cecb54a3a3d32c");
+    }
+
+    #[test]
+    fn load_pollapo_yml_should_load_deps() {
+        let pollapo_yml = load_pollapo_yml(Some("pollapo.test.yml"));
+        assert_eq!(pollapo_yml.deps[0], "pbkit/interface-pingpong-server@main");
     }
 }
