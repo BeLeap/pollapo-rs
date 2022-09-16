76469e35367d501dee200e05e2752c368e5c61be
Changseo <changseo.jang@riiid.co>
2022-09-16 14:26:29 +0900

    add empty install command

--- src/cmds/mod.rs
+++ src/cmds/mod.rs
@@ -1,10 +1,12 @@
 use clap::Subcommand;
 
-use self::add::AddArgs;
+use self::{add::AddArgs, install::InstallArgs};
 
 pub mod add;
+pub mod install;
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
     Add(AddArgs),
+    Install(InstallArgs),
 }
