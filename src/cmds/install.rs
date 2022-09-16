a897657867de5e663e63233f4f6246f706db80a7
Changseo <changseo.jang@riiid.co>
2022-09-16 16:18:40 +0900

    add install dep failing test

--- src/cmds/install.rs
+++ src/cmds/install.rs
@@ -5,5 +5,18 @@ use clap::Parser;
 pub struct InstallArgs {}
 
 pub fn action() {
-    todo!();
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    #[test]
+    fn install_dep_should_store_dep_zip_to_target() {
+        install_dep("pbkit/interface-pingpong-server@main", Some("cache_test"));
+        let temp = std::fs::read_dir("cache_test").unwrap()
+            .map(|entry| entry.unwrap().path()).collect::<Vec<PathBuf>>()
+            .contains(&std::path::PathBuf::from("./cache_test/pbkit/interface-pingpong-server@main.zip"));
+    }
 }
